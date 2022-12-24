38339699008fd7e25274d20ca5f56febdd079da1
rohankumarmainali <rohanmainali39@gmail.com>
2022-12-24 17:02:17 +0545

    gmail connect

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,13 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./utils/connectDB');
-import { Request, Response, NextFunction } from "express";
+const session = require('express-session');
+import {Express, Request, Response, NextFunction } from "express";
 
 const passport = require('passport');
 // server config
-const app = express();
-require('./config/passportSetup.ts')
+const app : Express = express();
+require('./config/passportSetup.ts');
 
 const PORT = process.env.PORT;
 // database connection
@@ -19,10 +20,15 @@ const router  = require('./routes/index.routes');
 app.use(express.json());
 app.use('/api',router);
 
+app.get('/dashboard',(req:Request,res:Request)=>{console.log('logged in')})
+app.get('/login',(req:Request,res:Request)=> console.log('error'));
 
-app.get('/google', passport.authenticate('google',{scope:['profile','email']}))
+app.get('/google', passport.authenticate('google',{scope:['profile','email']}, (req:Request,res:Response)=>{}))
 
-
+app.get('/google/callback', passport.authenticate( 'google', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/login'
+}));
 
 
 app.listen(PORT, ()=>console.log(`I am running at http://localhost:${PORT}`))
--- src/config/passportSetup.ts
+++ src/config/passportSetup.ts
@@ -7,7 +7,7 @@ const googleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new googleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
-    callbackURL:process.env.CALLBACK_URL,
+    callbackURL: "http://localhost:5000/google/callback",
     passReqToCallback : true
 }, (request:any, accessToken:String, refreshToken:String, profile:any,done:any) =>{
     console.log(profile);
