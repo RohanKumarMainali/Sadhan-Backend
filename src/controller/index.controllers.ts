13ff881f4f8d1a5a8a9ab989e999ad0b8c8bf91b
984345 <rohanmainali39@gmail.com>
2022-12-28 09:50:41 +0545

    added user controllers

--- src/controller/index.controllers.ts
+++ src/controller/index.controllers.ts
@@ -1,6 +1,6 @@
 const controllers = {
     adminControllers: require("./admin/index.controller"),
-    dashboard: require("./user/index.controller")
+    userControllers: require("./user/index.controller")
    }
    
 module.exports = controllers;
