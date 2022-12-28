53143dbff35254171745af2123ff9ef697231b4d
984345 <rohanmainali39@gmail.com>
2022-12-28 09:53:19 +0545

    user routes config

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,24 +1,29 @@
-const route = require('express').Router()
-const {dashboard} = require('../controller/index.controllers').dashboard;
-const session = require('express-session')
-import {Express, Request, Response, NextFunction } from "express";
+const route = require("express").Router();
+const session = require("express-session");
+const userModel = require('../models/user.model.ts');
+import { Express, Request, Response, NextFunction } from "express";
 
-const passport = require('passport');
-// server config
+// signup and login
 
-const CLIENT_URL = 'http://localhost:3000/';
-route.get('/dashboard',dashboard);
+const {login,signup} = require('../controller/index.controllers').userControllers;
+route.post('/user/signup',signup);
 
-route.get('/login/success',(req:any,res:any)=>{
-    if(req.user){
-       res.status(200).json({
-           success:true,
-           message: "successfull",
-           user: req.user,
-       });
-    }
-    else res.status(400).send({message:'sorry'})
-})
+route.post('/user/login',login);
+
+// social auth 
+const passport = require("passport");
+
+const CLIENT_URL = "http://localhost:3000/";
+
+route.get("/login/success", (req: any, res: any) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+    });
+  } else res.status(400).send({ message: "sorry" });
+});
 
 route.get("/login/failed", (req: any, res: any) => {
   res.status(401).json({
@@ -27,18 +32,27 @@ route.get("/login/failed", (req: any, res: any) => {
   });
 });
 
+route.get("/logout", (req: any, res: any) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
 
-route.get('/logout',(req:any,res:any)=>{
-    req.logout();
-    res.redirect(CLIENT_URL);
-})
+route.get(
+  "/google",
+  passport.authenticate(
+    "google",
+    { scope: ["profile", "email"] },
+    (req: Request, res: Response) => {}
+  )
+);
 
-route.get('/google', passport.authenticate('google',{scope:['profile','email']}, (req:Request,res:Response)=>{}))
-
-route.get('/google/callback', passport.authenticate( 'google', {
-        successRedirect: CLIENT_URL,
-        failureRedirect: '/login/failed'
-}));
+route.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
 //test
 
 module.exports = route;
