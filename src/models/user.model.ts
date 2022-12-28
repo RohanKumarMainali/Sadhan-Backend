c705aa681233f79e274b52c458580cca73fd4bbe
984345 <rohanmainali39@gmail.com>
2022-12-28 09:51:55 +0545

    added token

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,19 @@
 
 import mongoose from "mongoose"
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     firstName: 'string',
     lastName: 'string',
     email: 'string',
     password: 'string',
+    token: 'string',
+    createdOn: Date,
 })
+
+userSchema.methods.matchPassword = async function (password : String){
+    return await bcryptjs.compare(password,this.password)
+}
+
 
 module.exports = mongoose.model("users",userSchema)
