5a64688969c1a8883b2c2d5e214a5c9739af5b80
984345 <rohanmainali39@gmail.com>
2022-12-30 23:15:48 +0545

    login

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -7,43 +7,41 @@ import { STATUS_CODES } from "http";
 
 const GENERATE_JWT = async (uid: String) => {
   const accessToken = jwt.sign({ id: uid }, process.env.ACCESS_TOKEN_KEY, {
-    expiresIn: "30sec",
+    expiresIn: "5min",
   });
   const refreshToken = jwt.sign({ id: uid }, process.env.REFRESH_TOKEN_KEY);
   return {
     ACCESS_TOKEN: accessToken,
-    REFRESH_TOKEN : refreshToken,
+    REFRESH_TOKEN: refreshToken,
   };
 };
 
-const VERIFY_JWT = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const VERIFY_JWT = async (req: Request, res: Response, next: NextFunction) => {
+  let accessToken = req.header("authorization");
 
-    let accessToken = req.header('authorization');
+  // check if token is provided
 
-    // check if token is provided
+  if (accessToken === undefined || accessToken.length <= 9) {
+    return res.status(401).send({
+      success: false,
+      error: "token is empty!",
+    });
+  }
 
-    if(accessToken === undefined || accessToken.length <= 9){
-        return res.status(401).send({
-            success: false,
-            error: "token is empty!" 
-        })
-    }
+  // remove bearer from the accessToken to verify
 
-    // remove bearer from the accessToken to verify
-
-    accessToken = accessToken.substring(7,accessToken.length)
-    try {
-      const response = await jwt.verify(accessToken,process.env.ACCESS_TOKEN_KEY);  
-      next();
-    } catch (error) {
-        return res.status(401).send({
-            error: "Session Timeout!"
-        })
-    }
+  accessToken = accessToken.substring(7, accessToken.length);
+  try {
+    const response = await jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_KEY
+    );
+    next();
+  } catch (error) {
+    return res.status(401).send({
+      error: "Session Timeout!",
+    });
+  }
 };
 
-module.exports = {GENERATE_JWT , VERIFY_JWT};
+module.exports = { GENERATE_JWT, VERIFY_JWT };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,104 +1,95 @@
 const route = require("express").Router();
 const session = require("express-session");
-const userModel = require('../models/user.model.ts');
+const userModel = require("../models/user.model.ts");
 import { Express, Request, Response, NextFunction } from "express";
 
-import crypto from 'crypto';
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+import crypto from "crypto";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // signup and login
 
-const {login,signup} = require('../controller/index.controllers').userControllers;
-route.post('/user/signup',signup);
+const { login, signup } =
+    require("../controller/index.controllers").userControllers;
+route.post("/user/signup", signup);
 
-route.post('/user/login',login);
-
+route.post("/user/login", login);
 
 // sessions
 
-const JWT_SECRET = "{8367E87C-B794-4A04-89DD-15FE7FDBFF78}"
-const JWT_REFRESH_SECRET = "{asdfasdfdsfa-B794-4A04-89DD-15FE7FDBFF78}"
+const JWT_REFRESH_SECRET = "{asdfasdfdsfa-B794-4A04-89DD-15FE7FDBFF78}";
 
-route.get('/session',async (req: any,res: any)=>{
-    const token = req.cookies.JWT_TOKEN;
-    if(token){
-        
-        const user = await validateToken(token,JWT_SECRET); 
-
+route.get("/session",  (req: any, res: any) => {
+    const token = req.cookies.token;
+    console.log(token)
+    const secret = process.env.ACCESS_TOKEN_KEY;
+    if (token) {
+        const user =  validateToken(token, secret);
+        console.log('user '+user)
+        if(user ===null) res.status(400).send({success: false, message: 'token invalid or expired'})
+        else res.status(200).send({success: true, message: 'token verified'})
     }
-})
+        else res.status(400).send({success: false, message: 'no token '})
 
-// social auth 
+});
+
+// social auth
 const passport = require("passport");
 
 const CLIENT_URL = "http://localhost:3000/";
 
 route.get("/login/success", (req: any, res: any) => {
-  if (req.user) {
-    res.status(200).json({
-      success: true,
-      message: "successfull",
-      user: req.user,
-    });
-  } else res.status(400).send({ message: "sorry" });
+    if (req.user) {
+        res.status(200).json({
+            success: true,
+            message: "successfull",
+            user: req.user,
+        });
+    } else res.status(400).send({ message: "sorry" });
 });
 
 route.get("/login/failed", (req: any, res: any) => {
-  res.status(401).json({
-    success: false,
-    message: "failure",
-  });
+    res.status(401).json({
+        success: false,
+        message: "failure",
+    });
 });
 
 route.get("/logout", (req: any, res: any) => {
-  req.logout();
-  res.redirect(CLIENT_URL);
+    req.logout();
+    res.redirect(CLIENT_URL);
 });
 
 route.get(
-  "/google",
-  passport.authenticate(
-    "google",
-    { scope: ["profile", "email"] },
-    (req: Request, res: Response) => {}
-  )
+    "/google",
+    passport.authenticate(
+        "google",
+        { scope: ["profile", "email"] },
+        (req: Request, res: Response) => { }
+    )
 );
 
 route.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
+    "/google/callback",
+    passport.authenticate("google", {
+        successRedirect: CLIENT_URL,
+        failureRedirect: "/login/failed",
+    })
 );
+
 //test
 
-async function validateToken(token: String, secret: String) {
+async function validateToken(token: any, secret: any) {
     try {
-        const result  = jwt.verify(token, secret);
-      
+        const result = jwt.verify(JSON.stringify(token),JSON.stringify(secret));
         return {
-            "email": result.email,
-        }
-    }
-    catch(ex){
+            email: result.id,
+        };
+    } catch (ex) {
+        console.log(ex)
         return null;
     }
-   
-  
 }
- 
-async function randomString() {
-    return crypto.randomBytes(64).toString('hex');
-}
-function sha256(txt: any){
-    const secret = 'abcdefg';
-    const hash = crypto.createHmac('sha256', secret)
-                    .update(txt)
-                    .digest('hex');
-   return hash;
-}
-
 
 module.exports = route;
