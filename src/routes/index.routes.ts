88d8afe417fdfc8365bd1fc7829fbc4361fe93ba
RohanKumarMainali <rohanmainali39@gmail.com>
2023-02-26 19:24:40 +0545

    kyc routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -6,9 +6,11 @@ const Router = require('express').Router();
 const ADMIN_ROUTES = require('./admin.routes');
 const USER_ROUTES = require('./user.routes');
 const VEHICLE_ROUTES = require('./vehicle.routes')
+const KYC_ROUTES = require('./kyc.routes')
 
 Router.use(ADMIN_ROUTES);
 Router.use(USER_ROUTES);
 Router.use(VEHICLE_ROUTES);
+Router.use(KYC_ROUTES)
 
 module.exports = Router;
