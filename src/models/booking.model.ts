fa30e651bb30ac362ddb74125e5194d9198fd703
RohanKumarMainali <rohanmainali39@gmail.com>
2023-03-25 12:40:29 +0545

    bookin model

---
+++ src/models/booking.model.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+const Vehicle = require('./vehicle.model')
+const User = require('./user.model')
+
+const bookingSchema = new mongoose.Schema({
+    vehicleId : {type: mongoose.Schema.Types.ObjectId, ref: Vehicle},
+    userId : {type: mongoose.Schema.Types.ObjectId, ref: User},
+    status: {type: String, required: true},
+    startDate : {type: Date, required: true},
+    endDate : {type: Date, required: true},
+    createdOn : {type: Date, required: true},
+})
+
+module.exports = mongoose.model('booking', bookingSchema)
