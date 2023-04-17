dcb3d84e194e109555fa4d1b90239634e5c58d2c
RohanKumarMainali <rohanmainali39@gmail.com>
2023-04-17 14:04:24 +0545

    vehicle get by ownerID

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -4,10 +4,12 @@ const postVehicle = require('../controller/vehicle/index.controller.ts').postVeh
 const getVehicle = require('../controller/vehicle/index.controller.ts').getVehicle
 const deleteVehicle = require('../controller/vehicle/index.controller.ts').deleteVehicle
 const updateVehicle = require('../controller/vehicle/index.controller.ts').updateVehicle
+const getVehicleByUser = require('../controller/vehicle/index.controller.ts').getVehicleByUser
 const auth = require('../middleware/auth')
 
-vehicleRoute.post('/postVehicle',auth.VERIFY_JWT, postVehicle)
+vehicleRoute.post('/postVehicle', postVehicle)
 vehicleRoute.get('/getVehicle', getVehicle)
+vehicleRoute.get('/getVehicleByUser/:userId', getVehicleByUser)
 vehicleRoute.get('/getVehicle/:id', getVehicle)
 vehicleRoute.delete('/deleteVehicle/:id', deleteVehicle)
 vehicleRoute.put('/updateVehicle/:id', updateVehicle)
