15b5cc21a090db0d5f898ba19bfa50ec801da437
Rohan-Mainali <rohan.mainali@smaitic.com>
2023-07-20 17:54:05 +0545

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,11 @@ const connectDB = require("./utils/connectDB");
 const session = require("express-session");
 const cookieSession = require("cookie-session");
 const cors = require("cors");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const fileUpload = require('express-fileupload')
-const renewTokenInterval = require('./middleware/renewToken')
-const cron = require('node-cron')
+const fileUpload = require("express-fileupload");
+const renewTokenInterval = require("./middleware/renewToken");
+const cron = require("node-cron");
 import { Express, Request, Response, NextFunction } from "express";
 
 const passport = require("passport");
@@ -28,20 +28,20 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-    })
+  cors({
+    origin: ["https://sadhan-frontend.vercel.app/"],
+    credentials: true,
+  })
 );
 
 app.use(
-    cookieSession({
-        name: "session",
-        keys: ["rohan"],
-        maxage: 24 * 60 * 60 * 100,
-    })
+  cookieSession({
+    name: "session",
+    keys: ["rohan"],
+    maxage: 24 * 60 * 60 * 100,
+  })
 );
-app.use(fileUpload({ useTempFiles: true }))
+app.use(fileUpload({ useTempFiles: true }));
 
 app.use(cookieParser());
 
@@ -51,5 +51,7 @@ app.use("/api", router);
 
 // node - cron
 
-export const httpServer = app.listen(PORT, () => console.log(`I am running at http://localhost:${PORT}`));
+export const httpServer = app.listen(PORT, () =>
+  console.log(`I am running at http://localhost:${PORT}`)
+);
 export default app;
